43016eeb6ecc7e2c07d380bcbba0a0e200baf4d4
Kourosh <honar7@gmail.com>
2021-07-10 23:27:56 +0430

    add main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { GlobalStatic } from 'src/GlobalStatic';
 
 @Component({
   selector: 'app-root',
@@ -10,43 +8,13 @@ import { GlobalStatic } from 'src/GlobalStatic';
 })
 export class AppComponent {
   title = 'Guess My Nation';
-  player: Player;
-  playerName = '';
-
   constructor(
     public httpClient: HttpClient
   ) {
-    this.player = new Player('');
   }
 
   ngOnInit() {
-    this.playerName = 'Guest';
-    this.player = new Player('Guest');
     this.title = 'Guess My Nation';
   }
-
-  addPlayer() {
-    // const model: CreatePlayerModel('ali');
-    const headers = { 'content-type': 'application/json', 'accept': ' */*' }
-    this.httpClient.post<any>(
-      environment.serverUrl + GlobalStatic.CreatePlayer,
-      { name: this.playerName },
-      {
-        'headers': headers,
-      }).subscribe(
-        data => {
-          console.log(data);
-        }
-      );
-  }
-
-}
-
-
-export class Player {
-  constructor(_name: string) {
-    this.name = _name;
-  }
-  name: string;
 }
 
